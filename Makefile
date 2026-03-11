f5f5cf95d2f92d0d3369ebfba16c26417acb2df1
Aoniii <aoniii.contact@gmail.com>
2026-03-11 22:47:37 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,32 @@
+-include ./source.mk
+
+NAME			=	parser_tester
+
+SRCS			+=	main.c
+
+OBJS			=	$(SRCS:.c=.o)
+OBJS_DIR 		=	objects
+OBJS_PATH		=	$(addprefix $(OBJS_DIR)/, $(OBJS))
+
+CC			=	gcc
+CFLAGS			=	-Wall -Wextra -Werror
+
+$(OBJS_DIR)/%.o: %.c
+	mkdir -p $(@D)
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+$(NAME): $(OBJS_PATH)
+	$(CC) $(CFLAGS) $(OBJS_PATH) -o $(NAME)
+
+all: $(NAME)
+
+clean:
+	rm -rf $(OBJS_DIR)
+
+fclean: clean
+	rm -f $(NAME)
+	rm -rf out
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ parser.h
@@ -0,0 +1,28 @@
+#ifndef PARSER_H
+# define PARSER_H
+
+# include <stdlib.h>
+# include <stdbool.h>
+
+/**	Parser modes		*/
+# define MODE_POSIX		0x01
+# define MODE_GNU		0x02
+
+/**	Data types		*/
+# define FLAG_SHORT		0x10
+# define FLAG_LONG		0x20
+
+/**	Flag value types	*/
+# define FLAG_VALUE_REQUIRED	0x40
+# define FLAG_VALUE_OPTIONAL	0x80
+
+/**	Flag types		*/
+# define FLAG_TYPE_INT		0x0100
+# define FLAG_TYPE_STRING	0x0200
+# define FLAG_TYPE_FLOAT	0x0400
+# define FLAG_TYPE_BOOLEAN	0x0800
+
+/** Configuration		*/
+# define PARSER_MODE		MODE_POSIX
+
+#endif
