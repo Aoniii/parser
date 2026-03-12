752d40158350d2e4beadb639ef5a25c045ee9e7a
Aoniii <aoniii.contact@gmail.com>
2026-03-12 12:40:51 +0100

    [ADD] parser struct type and more option

--- parser.h
+++ parser.h
@@ -1,28 +1,46 @@
 #ifndef PARSER_H
 # define PARSER_H
 
-# include <stdlib.h>
-# include <stdbool.h>
-
 /**		Parser modes			*/
-# define MODE_POSIX				0x01
-# define MODE_GNU				0x02
+# define MODE_STRICT			0x01
+# define MODE_PERMISSIVE		0x02
 
 /**		Data types				*/
-# define FLAG_SHORT				0x10
-# define FLAG_LONG				0x20
+# define OPT_SHORT				0x10
+# define OPT_LONG				0x20
 
 /**		Flag value types		*/
-# define FLAG_VALUE_REQUIRED	0x40
-# define FLAG_VALUE_OPTIONAL	0x80
+# define OPT_VALUE_REQUIRED		0x40
+# define OPT_VALUE_OPTIONAL		0x80
 
 /**		Flag types				*/
-# define FLAG_TYPE_INT			0x0100
-# define FLAG_TYPE_STRING		0x0200
-# define FLAG_TYPE_FLOAT		0x0400
-# define FLAG_TYPE_BOOLEAN		0x0800
+# define TYPE_NONE				0x0000
+# define TYPE_BOOLEAN			0x0100
+# define TYPE_INT				0x0200
+# define TYPE_UINT				0x0400
+# define TYPE_FLOAT				0x0800
+# define TYPE_STRING			0x1000
+# define TYPE_CALLBACK			0x2000
 
-/**		Configuration			*/
-# define PARSER_MODE			MODE_POSIX
+/**		Special					*/
+# define OPT_INCREMENT			0x010000
+# define OPT_STOP_PARSING		0x020000
+
+/**		Parser struct			*/
+typedef struct	s_option {
+	char		short_opt;
+	const char	*long_opt;
+	int			flags;
+	void		*value;
+	const char	*help;
+}				t_option;
+
+typedef struct	s_parser {
+	t_option	*options;
+	int			opt_count;
+	int			mode;
+	char		**args;
+	int			args_count;
+}				t_parser;
 
 #endif
