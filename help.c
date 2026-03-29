1fff6eae49dfbea266d408cb9e52492f0c93f5d8
Aoni <99089480+Aoniii@users.noreply.github.com>
2026-03-29 20:45:06 +0200

    Fix formatting of option help output

--- help.c
+++ help.c
@@ -27,9 +27,9 @@ static void help(const t_parser_info info, const t_option *options) {
 		if (opt->short_opt && opt->long_opt)
 			snprintf(buf, sizeof(buf), "  -%c, --%-20s", opt->short_opt, opt->long_opt);
 		else if (opt->short_opt)
-			snprintf(buf, sizeof(buf), "  -%c", opt->short_opt);
+			snprintf(buf, sizeof(buf), "  -%c%24s", opt->short_opt, "");
 		else if (opt->long_opt)
-			snprintf(buf, sizeof(buf), "     --%-20s", opt->long_opt);
+			snprintf(buf, sizeof(buf), "      --%-20s", opt->long_opt);
 		else
 			continue;
 		printf("%s %s\n", buf, opt->help);
